f0173b430387b087fd08857696e8359f23ae9615
Dylan <alzatedylan@gmail.com>
2024-11-26 23:23:02 -0500

    Creación de la base de datos, Creacion de la carpeta Domain y creación de las interfaces de categoria y libro.

---
+++ Domain/Interfaces/ICategoriaService.cs
@@ -0,0 +1,13 @@
+﻿using ApilibrosFinal2024_2.Dal.Entities;
+
+namespace ApilibrosFinal2024_2.Domain.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<IEnumerable<Categoria>> GetCategoriasAsync();
+        Task<Categoria> GetCategoriaByIdAsync(Guid id);
+        Task<Categoria> CreateCategoriaAsync(Libro libro);
+        Task<Categoria> DeleteCategoriaAsync(Guid Id);
+        Task<Categoria> EditCategoriaAsync(Libro libro);
+    }
+}
---
+++ Domain/Interfaces/ILibroService.cs
@@ -0,0 +1,14 @@
+﻿using ApilibrosFinal2024_2.Dal.Entities;
+using System.Diagnostics.Metrics;
+
+namespace ApilibrosFinal2024_2.Domain.Interfaces
+{
+    public interface ILibroService
+    {
+        Task<IEnumerable<Libro>> GetLibrosAsync();
+        Task<Libro> GetLibroByIdAsync(Guid id);
+        Task<Libro> CreateLibroAsync(Libro libro);
+        Task<Libro> DeleteLibroAsync(Guid Id);
+        Task<Libro> EditLibroAsync(Libro libro);
+    }
+}
