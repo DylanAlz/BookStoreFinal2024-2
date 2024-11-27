9619a33b1f595e409bd5e707f9306bf80aa6bdbc
Dylan <alzatedylan@gmail.com>
2024-11-26 23:47:12 -0500

    Interfaces inplementadas aún sin lógica.

---
+++ Domain/Services/CategoriaService.cs
@@ -0,0 +1,33 @@
+﻿using ApilibrosFinal2024_2.Dal.Entities;
+using ApilibrosFinal2024_2.Domain.Interfaces;
+
+namespace ApilibrosFinal2024_2.Domain.Services
+{
+    public class CategoriaService : ICategoriaService
+    {
+        public Task<Categoria> CreateCategoriaAsync(Libro libro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Categoria> DeleteCategoriaAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Categoria> EditCategoriaAsync(Libro libro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Categoria> GetCategoriaByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Categoria>> GetCategoriasAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Services/LibroService.cs
@@ -0,0 +1,33 @@
+﻿using ApilibrosFinal2024_2.Dal.Entities;
+using ApilibrosFinal2024_2.Domain.Interfaces;
+
+namespace ApilibrosFinal2024_2.Domain.Services
+{
+    public class LibroService : ILibroService
+    {
+        public Task<Libro> CreateLibroAsync(Libro libro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Libro> DeleteLibroAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Libro> EditLibroAsync(Libro libro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Libro> GetLibroByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Libro>> GetLibrosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
